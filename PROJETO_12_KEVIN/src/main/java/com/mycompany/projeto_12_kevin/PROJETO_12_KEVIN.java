290b3c9b756ba6cfb664c2f6e506c3682738ccd0
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-08-28 20:56:15 -0300

    Exercicio 12

--- PROJETO_12_KEVIN/src/main/java/com/mycompany/projeto_12_kevin/PROJETO_12_KEVIN.java
+++ PROJETO_12_KEVIN/src/main/java/com/mycompany/projeto_12_kevin/PROJETO_12_KEVIN.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.projeto_0012_kevin;
+package com.mycompany.projeto_12_kevin;
 
 import java.util.Scanner;
 
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author k.andrade
  */
-public class PROJETO_0012_KEVIN {
+public class PROJETO_12_KEVIN {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
@@ -22,10 +22,7 @@ public class PROJETO_0012_KEVIN {
         
         //Jogador dois
         String nomeJogadorDois;
-        String tentativaJogadorDois;
-        int tentativas = 1;
-        
-        
+      
         //Perguntas jogador um
         System.out.print("Digite seu nome jogador um: ");
         nomeJogadorUm = ler.next();
@@ -52,17 +49,27 @@ public class PROJETO_0012_KEVIN {
         nomeJogadorDois = ler.next();
         
         System.out.println("---------------------------");
-        System.out.println("VAMOS AS PISTAS, " + nomeJogadorDois.toUpperCase() + "!");
+        System.out.println("VAMOS AS TENTATIVAS, " + nomeJogadorDois.toUpperCase() + "!");
         System.out.println("---------------------------");
+        
+        Jogo(filmeJogadorUm, pistas);
+    }
+    
+    
+    public static void Jogo(String filme, String[] pistas) {
+        Scanner ler = new Scanner(System.in); 
+        String tentativaJogadorDois;
+        int tentativas = 1;
         
         for (int i = 0; i <= 4; i++) {
             System.out.println("Dica: " + pistas[i]);
-            System.out.print("Seu chute: ");
+            System.out.print("Seu tentativa: ");
             tentativaJogadorDois = ler.next();
             tentativaJogadorDois.toLowerCase();
             
-            if (filmeJogadorUm.equals(tentativaJogadorDois)) {
+            if (filme.equals(tentativaJogadorDois)) {
                 System.out.println("PARABENS! Voce conseguiu acertar o filme com " + tentativas + " tentativas.");
+                System.out.println("---------------------------");
                 break;
             }
             else {
@@ -71,5 +78,4 @@ public class PROJETO_0012_KEVIN {
             System.out.println("---------------------------");
         }
     }
-    
 }
