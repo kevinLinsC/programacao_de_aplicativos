ceebaefe3b9edb54579ac2f3ab2c46db9e850a7c
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-09-03 16:17:45 -0300

    projeto 13

---
+++ PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_13_kevin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kevin
+ */
+public class PROJETO_13_KEVIN {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        int indice;
+        
+        System.out.print("Digite o tamanho do vetor: ");
+        indice = ler.nextInt();
+        
+        int[] num = new int[indice];
+        
+        
+    }
+}
