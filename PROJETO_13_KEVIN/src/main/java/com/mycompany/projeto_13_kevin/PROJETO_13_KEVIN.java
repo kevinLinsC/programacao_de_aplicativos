a110bdce2f7735e92d5481197e6c5ce64345349f
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-09-03 21:15:25 -0300

    Incremento do projeto 13

--- PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
+++ PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.projeto_13_kevin;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -16,12 +18,63 @@ public class PROJETO_13_KEVIN {
         Scanner ler = new Scanner(System.in);
         
         int indice;
+        int i;
+        String operacao;
+        int resultado;
+        int num1;
+        int num2;
         
+        //Tamanho do vetor
         System.out.print("Digite o tamanho do vetor: ");
         indice = ler.nextInt();
         
-        int[] num = new int[indice];
+        //Cria o vetor com o tamanho que o usuario indicou
+        int[] numeros = new int[indice];
+        
+        //Pega os numeros e coloca dentro do vetor
+        for (i = 0; i < numeros.length; i++) {
+            System.out.print("Digite o numero da " + (i + 1) + " posicao: ");
+            numeros[i] = ler.nextInt();
+        }
+        
+        //Usuario escolhe a operacao que será feito
+        while (true) {
+            System.out.print("Escolha a operacao (+ | - | * | /): ");
+            operacao = ler.next();
+            if (operacao.equals("/") || operacao.equals("*") || operacao.equals("+") || operacao.equals("-")) {
+                break;
+            }
+        }
+        
+        //Escreve a posicao de cada numero
+        for (i = 0; i < numeros.length; i++) {
+            System.out.println("posicao " + (i + 1) + ": " + numeros[i]);
+        }
+        
+        //Primeiro numero
+        System.out.print("Escolha o primeiro indice: ");
+        num1 = numeros[ler.nextInt() - 1];
+        
+        //Segundo numero
+        System.out.print("Escolha o segundo indice: ");
+        num2 = numeros[ler.nextInt() - 1];
+        
+        switch (operacao) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "/":
+                resultado = num1 / num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+        }
         
         
+        System.out.print(Arrays.toString(numeros));
     }
 }
