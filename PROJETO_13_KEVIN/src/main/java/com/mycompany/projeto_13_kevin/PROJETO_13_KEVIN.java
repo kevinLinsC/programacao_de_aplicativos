3afe34b559444a3977e383e2ec9f8273a07195f6
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-09-04 21:47:05 -0300

    projeto 13-14

--- PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
+++ PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
@@ -19,10 +19,14 @@ public class PROJETO_13_KEVIN {
         
         int indice;
         int i;
+        int cont_resultados = 0;
         String operacao;
-        int resultado;
+        int resultado = 0;
         int num1;
         int num2;
+        
+        cont_resultados++;
+        int[] resultados = new int[cont_resultados];
         
         //Tamanho do vetor
         System.out.print("Digite o tamanho do vetor: ");
@@ -34,7 +38,14 @@ public class PROJETO_13_KEVIN {
         //Pega os numeros e coloca dentro do vetor
         for (i = 0; i < numeros.length; i++) {
             System.out.print("Digite o numero da " + (i + 1) + " posicao: ");
-            numeros[i] = ler.nextInt();
+            String num = ler.next();
+            try {
+                numeros[i] = Integer.parseInt(num);
+            } catch (NumberFormatException e) {
+                System.out.println("ERRO");
+                i--;
+            }
+            
         }
         
         //Usuario escolhe a operacao que será feito
@@ -53,27 +64,24 @@ public class PROJETO_13_KEVIN {
         
         //Primeiro numero
         System.out.print("Escolha o primeiro indice: ");
-        num1 = numeros[ler.nextInt() - 1];
+        num1 = ler.nextInt();
+        num1 = numeros[num1 - 1];
         
         //Segundo numero
         System.out.print("Escolha o segundo indice: ");
-        num2 = numeros[ler.nextInt() - 1];
+        num2 = ler.nextInt();
+        num2 = numeros[num2 - 1];
         
         switch (operacao) {
-            case "+":
-                resultado = num1 + num2;
-                break;
-            case "-":
-                resultado = num1 - num2;
-                break;
-            case "/":
-                resultado = num1 / num2;
-                break;
-            case "*":
-                resultado = num1 * num2;
-                break;
+            case "+" -> resultado = num1 + num2;
+            case "-" -> resultado = num1 - num2;
+            case "/" -> resultado = num1 / num2;
+            case "*" -> resultado = num1 * num2;
         }
         
+        resultados[cont_resultados] = resultado;
+        
+        System.out.println("A soma entre " + num1 + " e " + num2 + " = " + resultado);
         
         System.out.print(Arrays.toString(numeros));
     }
---
+++ PROJETO_14_KEVIN/src/main/java/com/mycompany/projeto_14_kevin/PROJETO_14_KEVIN.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_14_kevin;
+
+import java.io.DataInputStream;
+import java.util.Random;
+
+/**
+ *
+ * @author k.andrade
+ */
+public class PROJETO_14_KEVIN {
+
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+        String s;
+        DataInputStream dado;
+        int[][] num = new int[5][5];
+        int l;
+        int c;
+        int[] soma_coluna = new int[5];
+        int[] soma_linha = new int[5];
+        int soma_diag_prin = 0;
+        int soma_diag_seg = 0;
+        int soma;
+        
+        // Recebe os valores e mostra a matriz original
+        for (l = 0; l < 5; l++) {
+            for (c = 0; c < 5; c++) {
+                num[l][c] = aleatorio.nextInt(0, 10);
+                System.out.print(num[l][c] + " ");
+                if (num[l][c] < 10) {
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println("");
+        }
+        
+        System.out.println("----------------------------------");
+        
+        // Ordena a matriz de forma crescente
+        for (l = 0; l < 5; l++) {
+            for (c = 0; c < 5; c++) {
+                
+            }
+        }
+        
+        System.out.println("----------------------------------");
+        
+        // Soma de todas as linhas
+        for (l = 0; l < 5; l++) {
+            soma = 0;
+            for (c = 0; c < 5; c++) {
+                soma += num[l][c]; 
+            }
+            soma_coluna[l] = soma;
+            System.out.println("A soma da " + (l + 1) + " linha e: " + soma_coluna[l]);
+        }
+        
+        // Soma de todas as colunas
+        for (l = 0; l < 5; l++) {
+            soma = 0;
+            for (c = 0; c < 5; c++) {
+                soma += num[c][l]; 
+            }
+            soma_linha[l] = soma;
+            System.out.println("A soma da " + (l + 1) + " coluna e: " + soma_linha[l]);
+        }
+        
+        // Soma da diagonal principal
+        for (l = 0; l < 5; l++) {
+            int a = num.length - 1;
+            for (c = 0; c < 5; c++) {
+                soma_diag_prin += num[a][c];
+                a--;
+            }
+        }
+        
+        System.out.println("A soma da diagonal principal e: " + soma_diag_prin);
+    }
+}
