1cfb80940a1bb37d4ab003bf75d67a75bce882e3
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-09-06 16:30:34 -0300

    upgrade projeto 14

--- PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
+++ PROJETO_13_KEVIN/src/main/java/com/mycompany/projeto_13_kevin/PROJETO_13_KEVIN.java
@@ -5,7 +5,6 @@
 package com.mycompany.projeto_13_kevin;
 
 import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -13,20 +12,23 @@ import java.util.Scanner;
  * @author kevin
  */
 public class PROJETO_13_KEVIN {
-
+    
+    public static int[] B;
+    
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         
         int indice = 0; // Indice do vetor números.
         int i; // Variavel de indece, só para auxilios em for.
-        int cont_resultados = 1;
         String operacao; // Operação em que o calculo irá ser feito.
         int resultado = 0;
-        int num1 = 0;
-        int num2 = 0;
+        int esc1;
+        int esc2;
         String temp;
+        int cont_resultados = 1;
         
         int[] A = new int[1];
+        //int[] B = new int[cont_resultados];
         
         //Tamanho do vetor.
         while (indice <= 0) {
@@ -38,8 +40,6 @@ public class PROJETO_13_KEVIN {
                 System.out.println("Digite somente numeros.");
             }
         }
-        
-        
         
         System.out.println("-----------------------------------");
         
@@ -59,6 +59,8 @@ public class PROJETO_13_KEVIN {
             
         }
         
+        System.out.println("-----------------------------------");
+        
         //Usuario escolhe a operacao que será feito
         while (true) {
             System.out.print("Escolha a operacao (+ | - | * | /): ");
@@ -68,36 +70,43 @@ public class PROJETO_13_KEVIN {
             }
         }
         
+        System.out.println("-----------------------------------");
+        
         //Escreve a posicao de cada numero
         for (i = 0; i < numeros.length; i++) {
-            System.out.println("posicao " + (i + 1) + ": " + numeros[i]);
+            System.out.println((i + 1) + " Posicao: " + numeros[i]);
         }
         
+        System.out.println("-----------------------------------");
+        
         while (true) {
+            esc1 = -1;
+            esc2 = -1;
             //Primeiro numero
-            num1 = 0;
-            num2 = 0;
-            while (num1 < 1 && num1 > numeros.length) {
+            while (esc1 < 0 || esc1 >= numeros.length) {
                 System.out.print("Escolha o primeiro indice: ");
                 temp = ler.next();
                 try {
-                    num1 = Integer.parseInt(temp);
-                    num1 = numeros[num1 - 1];
+                    esc1 = Integer.parseInt(temp);
+                    esc1--;
+                    System.out.println("Indice " + esc1 + "; Numero: " + numeros[esc1]);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("Escolha um indice valido.");
                 } catch (NumberFormatException e) {
                     System.out.println("Digite somente numeros.");
                 }
-            }
+            }        
             
+            System.out.println("-----------------------------------");
         
             //Segundo numero
-            while (num2 <= 0) {
+            while (esc2 < 0 || esc2 >= numeros.length) {
                 System.out.print("Escolha o segundo indice: ");
                 temp = ler.next();
                 try {
-                    num2 = Integer.parseInt(temp);
-                    num2 = numeros[num2 - 1];
+                    esc2 = Integer.parseInt(temp);
+                    esc2--;
+                    System.out.println("Indice " + esc2 + "; Numero: " + numeros[esc2]);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("Escolha um indice valido.");
                 } catch (NumberFormatException e) {
@@ -106,27 +115,34 @@ public class PROJETO_13_KEVIN {
             }
         
             switch (operacao) {
-                case "+" -> resultado = num1 + num2;
-                case "-" -> resultado = num1 - num2;
-                case "/" -> resultado = num1 / num2;
-                case "*" -> resultado = num1 * num2;
+                case "+" -> resultado = numeros[esc1] + numeros[esc2];
+                case "-" -> resultado = numeros[esc1] - numeros[esc2];
+                case "/" -> resultado = numeros[esc1] / numeros[esc2];
+                case "*" -> resultado = numeros[esc1] * numeros[esc2];
             }
             
         
             A[cont_resultados - 1] = resultado;
             cont_resultados++;
             
-            int[] B = new int[cont_resultados];
+            
             B[cont_resultados - 1] = resultado;
+            
+            
             A = B;
+            
+            System.out.println("-----------------------------------");
         
-            System.out.println("A soma entre " + num1 + " e " + num2 + " = " + resultado);
+            System.out.println("A soma entre " + numeros[esc1] + " e " + numeros[esc2] + " = " + resultado);
         
-            System.out.println(Arrays.toString(numeros));
             System.out.println(Arrays.toString(A));
             System.out.println(Arrays.toString(B));
         }
         
+        
+    }
+    
+    public static void AdicionaVetor(int valor) {
         
     }
 }
--- PROJETO_14_KEVIN/src/main/java/com/mycompany/projeto_14_kevin/PROJETO_14_KEVIN.java
+++ PROJETO_14_KEVIN/src/main/java/com/mycompany/projeto_14_kevin/PROJETO_14_KEVIN.java
@@ -5,6 +5,7 @@
 package com.mycompany.projeto_14_kevin;
 
 import java.io.DataInputStream;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -18,18 +19,16 @@ public class PROJETO_14_KEVIN {
         String s;
         DataInputStream dado;
         int[][] num = new int[5][5];
-        int l;
-        int c;
-        int[] soma_coluna = new int[5];
-        int[] soma_linha = new int[5];
-        int soma_diag_prin = 0;
-        int soma_diag_seg = 0;
-        int soma;
+        int l; // Linha
+        int c; // Coluna
         
-        // Recebe os valores e mostra a matriz original
+        // Recebe os valores e mostra a matriz original.
+        Linha();
+        System.out.println("Matriz original: ");
+        Linha();
         for (l = 0; l < 5; l++) {
             for (c = 0; c < 5; c++) {
-                num[l][c] = aleatorio.nextInt(0, 10);
+                num[l][c] = aleatorio.nextInt(0, 50);
                 System.out.print(num[l][c] + " ");
                 if (num[l][c] < 10) {
                     System.out.print(" ");
@@ -39,55 +38,227 @@ public class PROJETO_14_KEVIN {
             System.out.println("");
         }
         
-        System.out.println("----------------------------------");
+        // Ordena a matriz de forma crescente.
+        num = OrdenaCres(num);
         
-        // Ordena a matriz de forma crescente
-        for (l = 0; l < 5; l++) {
-            for (c = 0; c < 5; c++) {
-                
-            }
-        }
         
-        System.out.println("----------------------------------");
+        // Soma linha e coluna.  
+        SomaLC(num);
         
-        // Soma de todas as linhas
+        // Encontra os números que apareceram mais de uma vez.
+        MaisDeUmaVez(num);
+
+        // Soma da diagonal principal e secundaria.
+        somaPS(num);
+        
+        // Substitui os numeros primos por -1.
+        NumerosPrimos(num);
+        
+        
+    }
+    
+    //Soma de linhas e colunas.
+    public static void SomaLC (int[][] num) {
+        int soma; // Variavel temporaria de soma.
+        int[] soma_coluna = new int[5];
+        int[] soma_linha = new int[5];
+        int l;
+        int c;
+        
+        System.out.println();
+        Linha();
+        System.out.println("As somas das linhas sao: ");
+        Linha();
+        
+        // Soma de todas as linhas.
         for (l = 0; l < 5; l++) {
             soma = 0;
             for (c = 0; c < 5; c++) {
                 soma += num[l][c]; 
             }
-            soma_coluna[l] = soma;
-            System.out.println("A soma da " + (l + 1) + " linha e: " + soma_coluna[l]);
+            soma_linha[l] = soma;
+            System.out.println("A soma da " + (l + 1) + " linha e: " + soma_linha[l]);
         }
         
-        // Soma de todas as colunas
+        System.out.println();
+        Linha();
+        System.out.println("As somas das colunas sao: ");
+        Linha();
+        
+        // Soma de todas as colunas.
         for (l = 0; l < 5; l++) {
             soma = 0;
             for (c = 0; c < 5; c++) {
                 soma += num[c][l]; 
             }
-            soma_linha[l] = soma;
-            System.out.println("A soma da " + (l + 1) + " coluna e: " + soma_linha[l]);
+            soma_coluna[l] = soma;
+            System.out.println("A soma da " + (l + 1) + " coluna e: " + soma_coluna[l]);
+        }
+    }
+    
+    // Soma da diagonal principal e secundaria.
+    public static void somaPS(int[][] num) {
+        int l;
+        int c;
+        int soma_diag_prin = 0;
+        int soma_diag_sec = 0;
+        
+        // Soma da diagonal principal.
+        System.out.println();
+        Linha();
+        System.out.println("Soma da diagonal principal e secundaria: ");
+        Linha();
+        for (l = 0; l < 5; l++) {
+            soma_diag_prin += num[l][l];
         }
         
-        // Soma da diagonal principal
+        // Soma da diagonal secundaria.
         for (l = 0; l < 5; l++) {
-            int a = num.length - 1;
+            c = num.length - 1 - l; // Pega o tamnho da matriz(5), diminui 1(4) e tira l
+            soma_diag_sec += num[l][c];
+        }
+        
+        System.out.println("A soma da diagonal principal e: " + soma_diag_prin);
+        System.out.println("A soma da diagonal secundaria e: " + soma_diag_sec);
+    }
+    
+    // Ordena a matriz.
+    public static int[][] OrdenaCres(int[][] num) {
+        int l;
+        int c;
+        int i = 0;
+        int j;
+        int[] num_vetor = new int[num.length * num.length];
+        // Ordena a matriz de forma crescente.
+        
+        // Coloca a matriz em um vetor
+        for (l = 0; l < 5; l++) {
             for (c = 0; c < 5; c++) {
-                soma_diag_prin += num[a][c];
-                a--;
+                num_vetor[i] = num[l][c];
+                i++;
             }
         }
         
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                divisiveis++;
-                if (divisiveis > 2) {
-                    break;
+        // Ordena o vetor
+        for (i = 0; i < num_vetor.length - 1; i++) {
+            for (j = i + 1; j < num_vetor.length; j++) {
+                if (num_vetor[i] > num_vetor[j]) {
+                    int temp = num_vetor[i];
+                    num_vetor[i] = num_vetor[j];
+                    num_vetor[j] = temp;
                 }
             }
         }
         
-        System.out.println("A soma da diagonal principal e: " + soma_diag_prin);
+        i = 0;
+        
+        System.out.println();
+        Linha();
+        System.out.println("Matriz ordenada de forma crescente: ");
+        Linha();
+        
+        // Coloca em uma matriz
+        for (l = 0; l < 5; l++) {
+            for (c = 0; c < 5; c++) {
+                num[l][c] = num_vetor[i];
+                i++;
+                System.out.print(num[l][c] + " ");
+                if (num[l][c] < 10) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+        
+        return num;
+    }
+    
+    // Encontra e mostra quais numeros apareceram mais de uma vez.
+    public static void MaisDeUmaVez(int[][] num) {
+        int l;
+        int c;
+        int i = 0;
+        int cont_repet; // Contagem de repetições.
+        int numero; // Variavel auxilar que guarda um numero.
+        int numero_passado = -1; // Guarda o ultimo numero.
+        int[] num_vetor = new int[num.length * num.length];
+        
+        // Coloca a matriz em um vetor
+        for (l = 0; l < 5; l++) {
+            for (c = 0; c < 5; c++) {
+                num_vetor[i] = num[l][c];
+                i++;
+            }
+        }
+        
+        System.out.println();
+        Linha();
+        System.out.println("Os numeros que se repetiram foram: ");
+        Linha();
+        
+        // Verifica se o numero é repetido ou não.
+        for (l = 0; l < num.length; l++) {
+            for (c = 0; c < num.length; c++) {
+                numero = num[l][c];
+                cont_repet = 0;
+                for (i = 0; i < num_vetor.length; i++) {
+                    if (numero == num_vetor[i] && numero != numero_passado) {
+                        cont_repet++;
+                    }
+                }
+                if (cont_repet > 1) {
+                    System.out.println("O numero " + numero + " se repete " + cont_repet + " vezes.");
+                    numero_passado = numero;
+                }
+            }
+        }
+    }
+    
+    public static void NumerosPrimos(int[][] num) {
+        int l;
+        int c;
+        int i;
+        int temp_num;
+        int divisiveis;
+        
+        // Verifica se há numeros primos na matriz.
+        for (l = 0; l < num.length; l++) {
+            for (c = 0; c < num.length; c++) {
+                temp_num = num[l][c];
+                divisiveis = 0;
+                // For para verificar se é primo.
+                if (temp_num > 0) {
+                    for (i = 1; i <= temp_num; i++) {
+                        if (temp_num % i == 0) {
+                            divisiveis++;
+                        }
+                    }
+                }
+                // Se for primo troca por -1.
+                if (divisiveis == 2) {
+                    num[l][c] = -1;
+                }
+            }
+        }
+        
+        System.out.println("");
+        Linha();
+        System.out.println("A matriz com os primos trocados por -1 sao: ");
+        Linha();
+        // Escreve a matriz atualizada.
+        for (l = 0; l < num.length; l++) {
+            for (c = 0; c < num.length; c++) {
+                System.out.print(num[l][c] + " ");
+                if (num[l][c] < 10 && num[l][c] >= 0) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+    
+    // Escreve uma linha.
+    public static void Linha() {
+        System.out.println("----------------------------------");
     }
 }
