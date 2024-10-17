3ae7368a7855e8506b2b1870c5cdff9da9560f50
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-10-17 15:59:42 -0300

    Projeto 17 pronto

---
+++ PROJETO_17_KEVIN/src/main/java/com/mycompany/projeto_17_kevin/PROJETO_17_KEVIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_17_kevin;
+
+/**
+ *
+ * @author kevin
+ */
+public class PROJETO_17_KEVIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
