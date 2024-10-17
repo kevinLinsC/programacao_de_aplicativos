0916c377f5f7fe5bbb02d76b500c44a147814164
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-10-17 16:02:33 -0300

    Adicionando dump do projeto 17

---
+++ PROJETO_17_KEVIN dump.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `musicas_boas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `musicas_boas`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: musicas_boas
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `musica`
+--
+
+DROP TABLE IF EXISTS `musica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `musica` (
+  `id_musica` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_musica` varchar(100) NOT NULL,
+  `cantor_musica` varchar(100) NOT NULL,
+  `genero_musica` varchar(50) NOT NULL,
+  `feat_musica` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_musica`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `musica`
+--
+
+LOCK TABLES `musica` WRITE;
+/*!40000 ALTER TABLE `musica` DISABLE KEYS */;
+INSERT INTO `musica` VALUES (1,'Family Matters','Drake','Hip-Hop',''),(2,'The Light','Juice WRLD','Hip-Hop',''),(4,'It\'s Up','Drake','Hip-Hop, Trap','Young Thug, 21 Savage'),(5,'HIGHJACK','A$AP Rocky','Hip-Hop','Jessica Pratt'),(7,'Timeless','The Weeknd','Hip-Hop','Playboi Carti'),(8,'PRIDE.','Kendrick Lamar','Hip-Hop','');
+/*!40000 ALTER TABLE `musica` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-17 16:02:05
