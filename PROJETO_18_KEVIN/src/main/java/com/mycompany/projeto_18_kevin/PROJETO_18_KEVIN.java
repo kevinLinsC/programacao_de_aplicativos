76b2225458a36c8bfb69ed45a59814ce9c863b6f
kevinLinsC <kevin.l.andrade@aluno.senai.br>
2024-11-06 21:52:54 -0300

    Projeto 18

---
+++ PROJETO_18_KEVIN/src/main/java/com/mycompany/projeto_18_kevin/PROJETO_18_KEVIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_18_kevin;
+
+/**
+ *
+ * @author k.andrade
+ */
+public class PROJETO_18_KEVIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
